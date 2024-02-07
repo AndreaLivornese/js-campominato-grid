73fba6af71e1291da46660d02a1fbb88964d97ad
andrea-livornese <andrealivornese@gmail.com>
2024-02-07 17:37:46 +0100

    bonus: aggiutno select per scegliere la difficoltà del gioco

--- js/script.js
+++ js/script.js
@@ -2,20 +2,34 @@
 
 const gridEl = document.querySelector("#grid");
 
+const hard = 100;
+const medium = 81;
+const easy = 49;
+let range = 0;
 
 document.querySelector("#btn-play").addEventListener("click",
     function(){
 
         gridEl.innerHTML= "";
 
-        const difficultyChoice = document.querySelector("#difficulty").value;
-        console.log(difficultyChoice);
+        const difficultyChoice = Number(document.querySelector("#difficulty").value);
 
-        for(let i=1; i<=100; i++){
+        if(difficultyChoice == 10){
+            range = hard;
+        }else if(difficultyChoice == 9){
+            range = medium;
+        }else{
+            range = easy;
+        }
+
+
+
+        for(let i=1; i<=range; i++){
             const newDivEl= document.createElement("div");
 
             newDivEl.innerText = i;
             newDivEl.classList.add("square");
+            newDivEl.style.setProperty("--my-split", difficultyChoice);
 
             newDivEl.addEventListener("click",
                 function(){
